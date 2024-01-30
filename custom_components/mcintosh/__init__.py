707a9bb7ef71e3cb72315bf4cd1e58d8531284a2
Ryan Snodgrass <rsnodgrass@gmail.com>
2024-01-30 11:13:57 -0800

    update

--- custom_components/mcintosh/__init__.py
+++ custom_components/mcintosh/__init__.py
@@ -1,12 +1,21 @@
 """
 McIntosh A/V for Home Assistant
 """
+import logging
+
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
+LOG = logging.getLogger(__name__)
+
 PLATFORMS = ['media_player']
+
+
+async def config_update_listener(hass, entry):
+    """Handle options update."""
+    LOG.error(f'Config options updates {entry.options}')
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEntry):
@@ -15,6 +24,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEnt
 
     # save under the entry id so multiple devices can be added to a single HASS
     hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # whenever config options are changed by user, callback config_update_lister
+    entry.async_on_unload(entry.add_update_listener(config_update_listener))
 
     # forward the setup to the media_player platform
     hass.async_create_task(
--- custom_components/mcintosh/config_flow.py
+++ custom_components/mcintosh/config_flow.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import logging
 import asyncio
-from typing import Any
+from typing import Any, Dict
 
 import voluptuous as vol
 from homeassistant import config_entries
@@ -11,6 +11,7 @@ from homeassistant import config_entries
 # from pyavcontrol.const import BAUD_RATES
 from homeassistant.components.zha import BAUD_RATES
 from homeassistant.const import CONF_NAME, CONF_URL
+from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import config_validation as cv
@@ -26,6 +27,8 @@ LOG = logging.getLogger(__name__)
 ERROR_CANNOT_CONNECT = {'base': 'cannot_connect'}
 ERROR_UNSUPPORTED = {'base': 'unsupported'}
 
+MCINTOSH_MODELS = []
+
 
 class UnsupportedError(HomeAssistantError):
     """Error for unsupported device types."""
@@ -36,12 +39,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle the initial step of selecting model to configure."""
-        errors: dict[str, str] | None = None
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return OptionsFlowHandler(config_entry)
 
+    @staticmethod
+    def supported_models():
         # load all the supported models from pyavcontrol and filter down to only McIntosh models
         supported_models = DeviceModelLibrary.create().supported_models()
 
@@ -51,10 +56,40 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # could also be a static list of models! (PROBABLY BEST OPTION)
         mcintosh_models = [x for x in supported_models if x.startswith('mcintosh')]
         # mcintosh_models = ['mcintosh_mx160']
+        return mcintosh_models
 
+    @staticmethod
+    def schema(supported_models):
+        return vol.Schema(
+            {
+                vol.Optional(CONF_NAME, default='McIntosh Audio'): cv.string,
+                vol.Required(CONF_MODEL): selector.SelectSelector(
+                    selector.SelectSelectorConfig(
+                        options=supported_models,
+                        mode=selector.SelectSelectorMode.DROPDOWN,
+                    )
+                ),
+                vol.Required(CONF_URL, default=DEFAULT_URL): cv.url,
+                vol.Optional(CONF_BAUD_RATE): vol.In(BAUD_RATES),
+            }
+        )
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step of selecting model to configure."""
+        errors: dict[str, str] | None = None
+
+        # NOTE: in future may need to be more selective to only include mcintosh_*
+        # that meet specific criteria...e.g. not all may be media players.
+        # Alternatively since new physical models are not released often, this
+        # could also be a static list of models! (PROBABLY BEST OPTION)
+        mcintosh_models = ConfigFlow.supported_models()
         LOG.info(f'Starting McIntosh config flow: {mcintosh_models}')
 
         if user_input is not None:
+            LOG.info(f'Handling user input: {user_input}')
+
             name = user_input.get(CONF_NAME).strip()
             model_id = user_input[CONF_MODEL]
             url = user_input.get(CONF_URL).strip()
@@ -85,6 +120,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors = ERROR_UNSUPPORTED
                 LOG.warning(f'Failed config_flow: {errors}', e)
             else:
+                # FIXME: this may be able to be simplified by just saving ALL user_input
+                # which is what I see some config flows doing.
                 return self.async_create_entry(
                     title=f'McIntosh {name}',
                     data={
@@ -98,18 +135,35 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # no user input yet, so display the form
         return self.async_show_form(
             step_id='user',
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(CONF_NAME, default='McIntosh Audio'): cv.string,
-                    vol.Required(CONF_MODEL): selector.SelectSelector(
-                        selector.SelectSelectorConfig(
-                            options=mcintosh_models,
-                            mode=selector.SelectSelectorMode.DROPDOWN,
-                        )
-                    ),
-                    vol.Required(CONF_URL, default=DEFAULT_URL): cv.url,
-                    vol.Optional(CONF_BAUD_RATE): vol.In(BAUD_RATES),
-                }
-            ),
+            data_schema=ConfigFlow.schema(mcintosh_models),
+            errors=errors,
+        )
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handles options flow for the component after it has already been setup."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        self._config_entry = config_entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] = None
+    ) -> dict[str, Any]:
+        """Manage the options for the custom component."""
+        errors: dict[str, str] = {}
+
+        supported_models = ConfigFlow.supported_models()
+
+        if user_input is not None:
+            # Validation and additional processing logic omitted for brevity.
+            # ...
+            if not errors:
+                # Value of data will be set on the options property of our config_entry
+                # instance.
+                return self.async_create_entry(title='', data=user_input)
+
+        return self.async_show_form(
+            step_id='init',
+            data_schema=ConfigFlow.schema(supported_models),
             errors=errors,
         )
